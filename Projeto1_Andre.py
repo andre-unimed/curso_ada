eae5eb87362d26daf45f49268cbe67fb9baf71b1
andre-unimed <andre-unimed>
2023-12-11 18:09:38 +0000

    v1.1

--- Projeto1_Andre.py
+++ Projeto1_Andre.py
@@ -5,25 +5,28 @@ def dois_dados() -> int:
     soma = np.sum(resultados)
     return soma
 
-jogos = np.array(dois_dados())
+#jogos = np.array(dois_dados())
 
 lancamentos = ""
 while type(lancamentos) is not int:
     try:
         lancamentos = int(input('Insira o número de lançamentos de dois dados: '))
+        assert lancamentos > 0
     except:
-        print('Por favor insira um número inteiro')
+        print('Por favor insira um número inteiro e maior que 0')
+        lancamentos = ""
 
-for x in range(lancamentos-1):
-    jogos = np.append(jogos,dois_dados())
+jogos = np.empty(lancamentos, dtype = int)
+
+for x in range(lancamentos):
+    jogos[x] = dois_dados()
 
 lancamento, ocorrencia = np.unique(jogos,return_counts=True)
 
-#print(jogos)
 print(f'A média dos resultados é: {jogos.mean()}')
 print(f'O lançamento máximo foi: {jogos.max()}')
 print(f'O lançamento mínimo foi: {jogos.min()}')
 print('Número de vezes de cada lançamento:')
+print('Valor - Ocorrências')
 for valor, qtd in zip(lancamento, ocorrencia):
-    print(f'{valor} - {qtd}')
-
+    print(f'{valor:5} - {qtd}')
